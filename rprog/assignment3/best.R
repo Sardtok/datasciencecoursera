0ca8f1334bbd0a0833ff25bd37c4edc6dbc8315f
Sigmund Hansen <sigmunha@ifi.uio.no>
2014-07-12 23:57:11 +0200

    Adds second part of assignment 3 (get best hospital).

---
+++ rprog/assignment3/best.R
@@ -0,0 +1,26 @@
+make.best <- function() {
+  hospital_data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
+  hospital_data <- hospital_data[,c(11,17,23,7,2)]
+  hospital_data[,1:3] <- suppressWarnings(sapply(hospital_data[,1:3], as.numeric))
+  types <- colnames(hospital_data)[1:3]
+  names(types) <- c("heart attack", "heart failure", "pneumonia")
+  
+  function(state,outcome) {
+    data <- hospital_data[hospital_data[,4] == state,]
+    
+    if (dim(data)[1] == 0) {
+      stop("invalid state")
+    }
+    
+    if (match(outcome, types, nomatch=FALSE)) {
+      stop("invalid outcome")
+    }
+    
+    outcome <- types[outcome]
+    
+    data <- data[,c(outcome, "Hospital.Name")]
+    data[order(data)[1],2]
+  }
+}
+
+best <- make.best()
